22f43e8a59a3f1affe17f8aeb24e4944a4f1b7f0
wja123 <w.j.alag@gmail.com>
2017-10-14 18:00:39 -0400

    Added image assets for the first Story

--- App.js
+++ App.js
@@ -11,11 +11,12 @@ import HowIFeel from './src/views/HowIFeel'
 import Home from './src/views/Home'
 import Stories from './src/views/Stories'
 import PainPoints from './src/components/PainPoints'
+import Story1 from './src/components/Story1'
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <HowIFeel />
+      <Story1 />
     );
   }
 }
---
+++ src/components/Story1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, ImageBackground } from 'react-native'
+
+export const Story1 = (props) => {
+  return(
+    <View>
+      <View>
+      <ImageBackground>
+      </ImageBackground>
+      </View>
+    </View>
+  )
+}
