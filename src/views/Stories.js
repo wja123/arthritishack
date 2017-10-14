c0a9b7a3efbe17166c6ed2e2baa8b2e861ef0973
Ian Duke <ian@thefarmventures.com>
2017-10-14 17:24:02 -0400

    render story images in FlatList

--- src/views/Stories.js
+++ src/views/Stories.js
@@ -1,17 +1,58 @@
 import React, { Component } from  'react';
-import {StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native'
+import {StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity, FlatList} from 'react-native'
 import {styles} from '../styles/styles'
 
 export default class Stories extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [{ id: 1, title: 'Symptoms', image:'https://raw.githubusercontent.com/wja123/arthritishack/258b1bb61442acf983b4dc0de8d7beacfe61fa3a/src/assets/1%20thumbnail%20symptoms.jpg'}, { id: 2, title: 'Medicines', image:'https://raw.githubusercontent.com/wja123/arthritishack/258b1bb61442acf983b4dc0de8d7beacfe61fa3a/src/assets/2%20thumbnail%20medicines.jpg'}, { id: 3, title: 'Play', image:'https://raw.githubusercontent.com/wja123/arthritishack/258b1bb61442acf983b4dc0de8d7beacfe61fa3a/src/assets/3%20thumbnail%20ridiing%20bike.jpg'}],
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false,
+    };
+  }
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#CED0CE",
+        }}
+      />
+    );
+  };
+
   render(){
     return (
-      <View style={[styles.container, {backgroundColor: 'orange'}]}>
-        <View style={[styles.viewHeader, {backgroundColor: 'purple'}]}>
-          <Text style={styles.viewHeaderText}>Stories</Text>
+      <View style={[styles.container]}>
+      <View style={[styles.viewHeader]}>
+        <Text style={styles.viewHeaderText}>Stories</Text>
+      </View>
+      <View style={[styles.bodyContainer]}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <View>
+            <View>
+              <Image source={{uri: item.image}} style={{width: 200, height: 200, resizeMode: Image.resizeMode.contain}}>
+              </Image>
+              </View>
+              <View>
+              <Text>{item.title}</Text>
+              </View>
+            </View>
+          )}
+          ItemSeparatorComponent={this.renderSeparator}
+          keyExtractor={item => item.title}
+        />
         </View>
-        <View style={[styles.bodyContainer]}>
-          
-        </View>
+
       </View>
     )
   }
