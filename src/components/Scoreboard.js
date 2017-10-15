d7436d79f2481e54278550794bf0a1b7f049d030
wja123 <w.j.alag@gmail.com>
2017-10-15 00:09:36 -0400

    Updated Scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import {Image, ImageBackground} from 'react-native';
+import React, { Component } from  'react';
+import {StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity, FlatList} from 'react-native'
+import {styles} from '../styles/styles'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export const Scoreboard = (props) => {
-  return (
-    <Image style={{flex: 1}} source={{uri: 'https://raw.githubusercontent.com/wja123/arthritishack/master/src/assets/scoreboard/mockup.jpg'}}/>
-)
+export default class Scoreboard extends Component {
 
+  constructor(props) {
+    super(props);
+  };
+
+  render(){
+    return (
+      <View style={[styles.container]}>
+        <ImageBackground style={{flex: 1}} source={{uri: 'https://raw.githubusercontent.com/wja123/arthritishack/master/src/assets/scoreboard/mockup.jpg'}}>
+          <View style={{flexDirection: 'row'}}>
+            <View style={{width: '100%'}}>
+              <TouchableOpacity style={{backgroundColor: 'rgba(0,0,0,0)'}} onPress={() => { this.props.navView('HOME')}}>
+                <Icon name={'keyboard-arrow-left'} size={35} style={{marginTop: 25}} color={'black'} />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    )
+  }
 }
