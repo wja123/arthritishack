372da6f2efc52f9014f056a137cdc8c72954264b
Ian Duke <ian@thefarmventures.com>
2017-10-15 12:52:56 -0400

    add Join Space logo to Home component

--- src/views/Home.js
+++ src/views/Home.js
@@ -20,11 +20,17 @@ const menuItems = [{name: 'STORY TIME', image: 'https://raw.githubusercontent.co
 export const Home = (props) => {
   return (
     <View style={[styles.container, {backgroundColor: 'orange', justifyContent: 'center'}]}>
+
+    <View style={{backgroundColor: 'white', width: "100%", alignItems: "center"}}>
+      <Image source={{uri: 'https://raw.githubusercontent.com/wja123/arthritishack/bef840def889bbf322eb7d55595733dca142d812/src/assets/logo.png'}} style={{width: 200, height: 100, marginTop: 15, marginBottom: 5}}>
+      </Image>
+    </View>
+
       {
         <FlatList style={{width:'100%'}} data={menuItems}
           renderItem={({item, index}) => (
             <View key={item.name} style={{justifyContent: 'center', alignItems: 'center'}}>
-              <TouchableOpacity  style={[styles.buttonContainer, {height: 200}]} onPress={() => { props.navView(item.name)}}>
+              <TouchableOpacity  style={[styles.buttonContainer, {height: 200, width: 275, paddingLeft: 15, paddingRight: 15}]} onPress={() => { props.navView(item.name)}}>
                 <Image source={{uri: item.image}} style={{resizeMode: 'contain', width: '100%', height: '100%'}} />
               </TouchableOpacity>
             </View>)}
