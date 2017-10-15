48a7f4a3f336f44b05a5c0b5329364adf47fe938
Ian Duke <ian@thefarmventures.com>
2017-10-15 14:16:02 -0400

    update Progress view to contain HCP/parent view

--- src/views/Progress.js
+++ src/views/Progress.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Image, ImageBackground, View, TouchableOpacity} from 'react-native';
+import {Image, ImageBackground, View, TouchableOpacity, Text} from 'react-native';
 import {styles} from '../styles/styles'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -7,8 +7,12 @@ export default class Progress extends Component {
 
   constructor(props) {
     super(props);
+    this.state.bgImage ='https://raw.githubusercontent.com/wja123/arthritishack/master/src/assets/progress/progress.jpg'
   };
-
+  toggleBgImg()
+  {
+    this.setState({bgImage: 'https://raw.githubusercontent.com/wja123/arthritishack/master/src/assets/progress/parent_progress.jpg'})
+  }
   render(){
       return (
       <View style={[styles.container, {backgroundColor: '#ffe331'}]}>
@@ -21,6 +25,11 @@ export default class Progress extends Component {
             </View>
           </View>
         </ImageBackground>
+        <View style={{backgroundColor: "white", borderRadius: 8}}>
+        <TouchableOpacity onPress={this.toggleBgImg.bind(this)}>
+          <Icon name={'keyboard-arrow-up'} size={35} style={{marginTop: 10}} color={'black'} />
+        </TouchableOpacity>
+        </View>
       </View>
     )
   }
