16e367cba4a0c07420a1aed5099e893d08fb6be5
Ian Duke <ian@thefarmventures.com>
2017-10-15 00:21:11 -0400

    Add back button to progress component

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import {Image, ImageBackground} from 'react-native';
+import {Image, ImageBackground, View, TouchableOpacity} from 'react-native';
+import {styles} from '../styles/styles'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export const Progress = (props) => (
-  <Image style={{flex: 1, resizeMode: 'contain'}} source={{uri: 'https://raw.githubusercontent.com/wja123/arthritishack/master/src/assets/progress/Sample%20graph.001.jpeg'}}/>
+  <View style={[styles.container]}>
+    <ImageBackground style={{flex: 1}} resizeMode="contain" source={{uri: 'https://raw.githubusercontent.com/wja123/arthritishack/master/src/assets/progress/Sample%20graph.001.jpeg'}}>
+      <View style={{flexDirection: 'row'}}>
+        <View style={{width: '100%'}}>
+          <TouchableOpacity style={{backgroundColor: 'rgba(0,0,0,0)'}} onPress={() => { props.navView('HOME')}}>
+            <Icon name={'keyboard-arrow-left'} size={35} style={{marginTop: 25}} color={'black'} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
+  </View>
 )
