320f39ff38f1494c46a56e89bda36aa6a1035a53
Ian Duke <ian@thefarmventures.com>
2017-10-15 11:40:11 -0400

    embed YouTube videos in Resources

--- src/views/Resources.js
+++ src/views/Resources.js
@@ -1,14 +1,19 @@
 import React, { Component } from  'react';
-import {StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity, FlatList, Linking} from 'react-native'
+import {StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity, FlatList, Linking, ScrollView} from 'react-native'
 import {styles} from '../styles/styles'
 import {Story} from '../components/Story'
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import SegmentedControlTab from 'react-native-segmented-control-tab';
+import YTViewer from 'react-native-youtube';
 
 export default class Resources extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      selectedIndex: 0,
+      visibility0: true,
+      visibility1: false,
       data : [
         {
           id: 1,
@@ -31,6 +36,95 @@ export default class Resources extends Component {
     }
   };
 
+  handleIndexChange = (index) => {
+    this.setState({
+      ...this.state,
+      selectedIndex: index
+    });
+  }
+
+  switchVisibility = (index) => {
+    switch (index) {
+        case 0:
+            this.setState({visibility0: true, visibility1: false});
+            break;
+        case 1:
+            this.setState({visibility0: false, visibility1: true});
+            break;
+        default:
+            break;
+    }
+  }
+  linksFlatlist(){
+    return (
+      <FlatList
+        data={this.state.data}
+        renderItem={({ item, index }) => (
+          <View style={{paddingTop: 30, alignItems: 'center'}}>
+            <View style={{flexDirection: "row", justifyContent: 'center', paddingBottom: 30}}>
+              <TouchableOpacity
+                style={{width: 275,
+                  height: 40,
+                  borderRadius: 6,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'white'}}
+                  onPress={()=> Linking.openURL(item.url)}
+                  >
+                <Text style={{color: 'black', fontWeight: "bold"}}>{item.title}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+        ItemSeparatorComponent={this.renderSeparator}
+        keyExtractor={item => item.id}
+      />
+    )
+  }
+  videoPage(){
+    return (
+      <ScrollView>
+      <YTViewer
+        videoId="9OK6_OdWxTA"
+        play={true}
+        loop={true}
+
+        onError={e => this.setState({ error: e.error })}
+        onReady={e => this.setState({ isReady: true })}
+        onChangeState={e => this.setState({ status: e.state })}
+        onChangeQuality={e => this.setState({ quality: e.quality })}
+
+        style={{height: 300, marginTop: 30, borderColor: "white"}}>
+      </YTViewer>
+
+      <YTViewer
+        videoId="_OQ7hgr0KlY"
+        play={true}
+        loop={true}
+
+        onError={e => this.setState({ error: e.error })}
+        onReady={e => this.setState({ isReady: true })}
+        onChangeState={e => this.setState({ status: e.state })}
+        onChangeQuality={e => this.setState({ quality: e.quality })}
+
+        style={{height: 300, marginTop: 30, borderColor: "white"}}>
+      </YTViewer>
+
+      <YTViewer
+        videoId="FfwsLj97BOk"
+        play={false}
+        loop={true}
+
+        onError={e => this.setState({ error: e.error })}
+        onReady={e => this.setState({ isReady: true })}
+        onChangeState={e => this.setState({ status: e.state })}
+        onChangeQuality={e => this.setState({ quality: e.quality })}
+
+        style={{height: 300, marginTop: 30, borderColor: "white"}}>
+      </YTViewer>
+      </ScrollView>
+    )
+  };
   renderSeparator = () => {
     return (
       <View
@@ -57,30 +151,17 @@ export default class Resources extends Component {
           </TouchableOpacity>
         </View>
       </View>
+
+      <SegmentedControlTab
+      values={['Links', 'Videos']}
+      selectedIndex={this.state.selectedIndex}
+      onTabPress={this.handleIndexChange, this.switchVisibility}>
+      </SegmentedControlTab>
+
       <View style={[styles.bodyContainer, {backgroundColor: 'orange'}]}>
-        <FlatList
-          data={this.state.data}
-          renderItem={({ item, index }) => (
-            <View style={{paddingTop: 30, alignItems: 'center'}}>
-              <View style={{flexDirection: "row", justifyContent: 'center', paddingBottom: 30}}>
-                <TouchableOpacity
-                  style={{width: 275,
-                    height: 40,
-                    borderRadius: 6,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    backgroundColor: 'white'}}
-                    onPress={()=> Linking.openURL(item.url)}
-                    >
-                  <Text style={{color: 'black', fontWeight: "bold"}}>{item.title}</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          )}
-          ItemSeparatorComponent={this.renderSeparator}
-          keyExtractor={item => item.id}
-        />
+        {this.state.visibility0 ?  this.linksFlatlist() : this.videoPage()}
         </View>
+
       </View>
     )
   }
